fa7688336113aa4e6ef9df264c6b760c8f4d845f
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-02-25 09:37:35 +0100

    added submit message to form

--- Js/contact.js
+++ Js/contact.js
@@ -8,69 +8,51 @@ const emailError = document.querySelector("#emailError");
 const address = document.querySelector("#address");
 const addressError = document.querySelector("#addressError");
 const submitButton = document.querySelector("#submitButton");
+const submitMessage = document.querySelector(".form-submitted");
 
 function validateForm(event) {
   event.preventDefault();
 
+  // checks validation of each input
   if (checkLength(firstName.value, 0) === true) {
     nameError.style.display = "none";
-    // firstName.style.display = "none";
+    firstName.style.display = "none";
   } else {
     nameError.style.display = "block";
   }
 
   if (checkLength(subject.value, 9) === true) {
     subjectError.style.display = "none";
+    subject.style.display = "none";
   } else {
     subjectError.style.display = "block";
   }
 
   if (checkLength(address.value, 25) === true) {
     addressError.style.display = "none";
+    address.style.display = "none";
   } else {
     addressError.style.display = "block";
   }
 
   if (validatEmail(email.value) === true) {
     emailError.style.display = "none";
+    email.style.display = "none";
   } else {
     emailError.style.display = "block";
   }
 
-  // if (
-  //   checkLength(firstName.value, 0) === true &&
-  //   checkLength(subject.value, 9) === true &&
-  //   checkLength(address.value, 25) === true &&
-  //   validatEmail(email.value) === true
-  // ) {
-  //   submitButton.disabled = false;
-  // } else {
-  //   submitButton.disabled = true;
-  // }
-
-  // if (firstName.value.trim().length > 0) {
-  //   firstName.style.display = "none";
-  // } else {
-  //   nameError.style.display = "block";
-  // }
-
-  // if (subject.value.trim().length > 9) {
-  //   subjectError.style.display = "none";
-  // } else {
-  //   subjectError.style.display = "block";
-  // }
-
-  // if (email.value.trim().length > 0) {
-  //   emailError.style.display = "none";
-  // } else {
-  //   emailError.style.display = "block";
-  // }
-
-  // if (address.value.trim().length > 24) {
-  //   subjectError.style.display = "none";
-  // } else {
-  //   addressError.style.display = "block";
-  // }
+  //displays submission message if all true
+  if (
+    checkLength(firstName.value, 0) === true &&
+    checkLength(subject.value, 9) === true &&
+    checkLength(address.value, 25) === true &&
+    validatEmail(email.value) === true
+  ) {
+    submitMessage.style.display = "block";
+  } else {
+    submitMessage.style.display = "none";
+  }
 
   console.log("");
 }
@@ -86,6 +68,7 @@ function checkLength(value, len) {
   }
 }
 
+// validat email address
 function validatEmail(email) {
   const regEx = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
   const patternMatches = regEx.test(email);

ec463b38325bd488d830c88e057de4e3f4e149f8
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-02-26 13:24:55 +0100

    clean up code-removed unused code

--- Js/details.js
+++ Js/details.js
@@ -39,40 +39,11 @@ async function fetchProduct() {
     details.innerHTML += `<div class="details" >
                                   <h1> ${results.title}</h1>
                                   <img class="img" src=${results.images[1]} width="300px"/> 
-                                  <h2> ${results.description}</h2>
-                                  <h3> ${results.price}</h3>
-                                  <h3> ${results.rating} star rating</h3>
+                                  <p> ${results.description}</p>
+                                  <p> ${results.price}</p>
+                                  <p> ${results.rating} Star Rating</p>
                                   </div>
                                 `;
-
-    // details.innerHTML += `<div class="university">${name}
-    //                         <ul>
-
-    //                           <li>${name.country}</li>
-    //                           <li>${name.web_pages}</li>
-    //                           <li>${name.domains}</li>
-    //                         </ul>
-    //                       </div>`;
-
-    // for (let i = 0; i < results.length; i++) {
-    //   if (i === 25) {
-    //     break;
-    //   }
-
-    //   details.innerHTML += `<div class="university" data-id=${i}>
-    //                               <ul>
-    //                                 <li>
-    //                                 ${results[i].country}
-    //                                 </li>
-    //                                 <li>
-    //                                 ${results[i].web_pages}
-    //                                 </li>
-    //                                 <li>
-    //                                 ${results[i].domains}
-    //                                 </li>
-    //                               </ul>
-    //                           </div>`;
-    // }
   } catch (error) {
     console.log(error);
     details.innerHTML = alert("error", error);
@@ -80,33 +51,3 @@ async function fetchProduct() {
 }
 
 fetchProduct();
-
-// const details = document.querySelector(".details");
-
-// const queryString = document.location.search;
-
-// const params = new URLSearchParams(queryString);
-
-// const id = params.get("id");
-
-// console.log(id);
-
-// const url =
-//   "http://universities.hipolabs.com/search?name=marywood+university" + id;
-
-// async function makeApiCall() {
-//   try {
-//     const response = await fetch(url);
-
-//     const results = await response.json();
-
-//     console.log(results);
-
-//     details.innerHTML = "";
-//   } catch (error) {
-//     console.log(error);
-//     details.innerHTML = alert("error", error);
-//   }
-// }
-
-// makeApiCall();
--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,6 @@
 const ourProducts = document.querySelector(".productContainer");
-// const schoolWebsite = document.querySelector(".website");
 
 const url = "https://dummyjson.com/products";
-
-const search = document.querySelector("#search");
 
 async function makeApiCall() {
   try {
@@ -24,10 +21,10 @@ async function makeApiCall() {
         continue;
       }
 
-      ourProducts.innerHTML += `<div class="productContainer" >
+      ourProducts.innerHTML += `<div class="product-card"  >
                                   <a href="details.html?id=${items[i].id}" ><h1> ${items[i].title}</h1></a>
                                   <a href="details.html?id=${items[i].id}"> <img class="img" src=${items[i].images[1]} width="300px"/> </a>
-                                  <a href="details.html?name=${items[i].price}"><h2> ${items[i].price} </h2></a>
+                                  <a href="details.html?name=${items[i].price}"><h2>$${items[i].price} </h2></a>
                                   </div>
                                 `;
     }
@@ -38,71 +35,3 @@ async function makeApiCall() {
 }
 
 makeApiCall();
-
-// search.onkeyup = function () {
-//   const searchValue = event.target.value.trim().toLowerCase();
-
-//   const filterSchools = universityName.filter(function (uni) {
-//     if (uni.full_name.toLowerCase().startsWith(searchValue)) return true;
-//   });
-// };
-
-// const schoolName = document.querySelector(".university");
-// // const schoolWebsite = document.querySelector(".website");
-
-// const url = "http://universities.hipolabs.com/search?country=United+States";
-
-// const search = document.querySelector("#search");
-
-// async function makeApiCall() {
-//   try {
-//     const response = await fetch(url);
-
-//     const results = await response.json();
-
-//     console.log(results);
-
-//     schoolName.innerHTML = "";
-
-//     // my api doesnt have id's so I am trying to addeventlistner to help target them
-//     document.addEventListener("click", (e) => {
-//       if (e.target.dataset.id) {
-//         console.log(e.target.dataset.id);
-
-//         console.log(results[e.target.dataset.id]);
-//       }
-//     });
-
-//     for (let i = 0; i < results.length; i++) {
-//       if (i === 25) {
-//         break;
-//       }
-
-//       schoolName.innerHTML += `<div class="university" data-id=${i}>
-//                                   <a href="details.html?name=${results[i].name}" > ${results[i].name}</a>
-//                                   <ul>
-//                                     <li>
-//                                     ${results[i].web_pages}
-//                                     </li>
-//                                     <li>
-//                                     ${results[i].domains}
-//                                     </li>
-//                                   </ul>
-//                                 </div>
-//                                 `;
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     schoolName.innerHTML = alert("error", error);
-//   }
-// }
-
-// makeApiCall();
-
-// search.onkeyup = function () {
-//   const searchValue = event.target.value.trim().toLowerCase();
-
-//   const filterSchools = universityName.filter(function (uni) {
-//     if (uni.full_name.toLowerCase().startsWith(searchValue)) return true;
-//   });
-// };
